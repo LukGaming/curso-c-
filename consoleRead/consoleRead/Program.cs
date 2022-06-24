f9b890f2c2df53e7f2d0ca36fd6090373144035c
@Paulo.luk <=paulo.mendes@fmxsolucoes.com.br>
2022-06-24 14:33:34 -0300

    testes com console write

---
+++ consoleRead/consoleRead/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite seu nome:");
+string nome = Console.ReadLine();
+Console.WriteLine($"Olá, {nome}");
+Console.WriteLine($"Digite o primeiro nome de jogadores separado por espaço ");
+string players = Console.ReadLine();
+string[] players_list = players.Split(' ');
+for (int i = 0; i < players_list.Length; i++)
+{
+    Console.WriteLine($"{i}: {players_list[i]}");
+}
